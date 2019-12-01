9d3c919fde9d261eb20efccf6d723496212f4640
“Mison” <mison4uk@gmail.com>
2019-12-01 23:26:35 +0200

    Задання виконане

--- homeworkfor.php
+++ homeworkfor.php
@@ -15,21 +15,36 @@ $dob=0;
 <head>
     <title>Основы языка PHP</title>
     <style>
-
+        th {
+            background: yellow; /* Цвет фона строки заголовка */
+        }
     </style>
 </head>
 <body>
 <h1>Цикл for</h1>
-<table>
+<table border="1" width="300">
+    <caption><h1>Таблиця множення</h1></caption>
+        <tr>
+            <th> Число 1 </th>
+            <th> Число 2 </th>
+            <th> Добуток </th>
+        </tr>
+
     <?php
     // Решение
     for ($i=1;$i<=9;$i++){
         for ($j=1;$j<=9;$j++){
             $dob=$i*$j;
-            echo "$i" . " * " . "$j" . " = " . "$dob";
+    ?>
+        <tr>
+            <td bgcolor="yellow"> <?php echo $i ?> </td>
+            <td> <?php echo $j ?> </td>
+            <td> <?php echo $dob ?> </td>
+        </tr>
+    <?php
         }
     }
-     ?>
+    ?>
 </table>
 </body>
 </html>
